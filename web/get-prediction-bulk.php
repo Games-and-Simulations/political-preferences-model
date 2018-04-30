f4e017c465466226b59555b78ee8216f64a61bc2
Kamil Svec <sveckami@fel.cvut.cz>
2018-04-30 15:04:17 +0200

    sending data in bulk

---
+++ web/get-prediction-bulk.php
@@ -0,0 +1,36 @@
+<?php
+require "connectdb.php";
+
+$json = json_decode(file_get_contents('php://input'));
+
+$idArray = $json->idArray;
+$modelid = $json->modelid;
+
+if (!ctype_digit($modelid)) {
+	echo "no-results";
+	exit();	
+}
+
+$tablename = "model" . $modelid;
+$jsonArray = array();
+
+foreach ($idArray as $id) {
+	$stmt = $db->prepare('SELECT * FROM '. $tablename .' AS s WHERE s.obec=? AND s.momc=? AND s.okrsek=?;');
+	$stmt->bindParam(1, $id[0]);
+	$stmt->bindParam(2, $id[1]);
+	$stmt->bindParam(3, $id[2]);
+	$stmt->execute();
+	
+	if ($stmt) {
+		$row = $stmt->fetch(PDO::FETCH_ASSOC);
+		if($row == false) {
+			echo "no-results";
+		} else {
+			$row["type"] = $tablename;
+			array_push($jsonArray, json_encode($row));
+		}
+	}
+}	
+$jsonObject["predictionArray"] = $jsonArray;
+echo json_encode($jsonObject);
+?>
---
+++ web/get-result2017-bulk.php
@@ -0,0 +1,31 @@
+<?php
+require "connectdb.php";
+
+$json = json_decode(file_get_contents('php://input'));
+
+$idArray = $json->idArray;
+
+$jsonArray = array();
+
+foreach ($idArray as $id) {
+	$stmt = $db->prepare('SELECT id, obec, momc, okrsek, pocet_volicu, ucast, other, ods,
+					cssd, stan, kscm, zeleni, svobodni, pirati, top09, ano, kducsl, spd
+					FROM public.snemovna2017 AS s WHERE s.obec=? AND s.momc=? AND s.okrsek=?;');
+	$stmt->bindParam(1, $id[0]);
+	$stmt->bindParam(2, $id[1]);
+	$stmt->bindParam(3, $id[2]);
+	$stmt->execute();
+	
+	if ($stmt) {
+		$row = $stmt->fetch(PDO::FETCH_ASSOC);
+		if($row == false) {
+			echo "no-results";
+		} else {
+			$row["type"] = "snemovna2017";
+			array_push($jsonArray, json_encode($row));
+		}
+	}
+}	
+$jsonObject["predictionArray"] = $jsonArray;
+echo json_encode($jsonObject);
+?>
